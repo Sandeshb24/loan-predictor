9e108c80714886abce93726eab47795eb5a61a1d
Sandesh R Bhat <96337802+Sandeshb24@users.noreply.github.com>
2025-06-28 15:32:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
 
 def load_and_preprocess_data(file_path="https://raw.githubusercontent.com/Sandeshb24/loan-predictor/refs/heads/main/loan_approval_dataset.csv"):
     """
